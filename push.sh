0328b84e838920b685698f543b44f174189618e7
huaxvcode <huaxvcode@yeah.net>
2023-09-01 08:40:02 +0800

    逆元.md

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdocs -q build && git add . && git commit -m "%*" && git push -u origin master -f
+mkdocs -q build && git add . && git commit -m "$*" && git push -u origin master -f
 
 # 强制推送，丢弃远程
