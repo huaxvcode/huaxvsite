530ea33489ba0d2a02e24d25dee12dbe5c6a53a0
huaxvcode <huaxvcode@yeah.net>
2023-11-21 16:37:34 +0800

    linux 学习笔记.md

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
 
-mkdocs -q build && git add . && git commit -m "%*" && git push -u origin master -f
+@REM mkdocs -q build && git add . && git commit -m "%*" && git push -u origin master -f
 
-@REM 强制推送，丢弃远程
+mkdocs -q build && git add . && git commit -m "%*" && git push
